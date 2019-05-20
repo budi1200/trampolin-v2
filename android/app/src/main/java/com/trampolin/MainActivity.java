0e1c4887c4dd3782268a4e11caef9288955b9963
budi1200 <alenab22@gmail.com>
2019-05-20 14:46:52 +0200

    splash screen android

--- android/app/src/main/java/com/trampolin/MainActivity.java
+++ android/app/src/main/java/com/trampolin/MainActivity.java
@@ -1,6 +1,27 @@
 package com.trampolin;
 
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.widget.LinearLayout;
+
 import com.reactnativenavigation.NavigationActivity;
 
+
+
 public class MainActivity extends NavigationActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(this.createSplashLayout());
+
+    }
+
+    public LinearLayout createSplashLayout() {
+        LinearLayout splash = new LinearLayout(this);
+        Drawable launch_screen_bitmap = ContextCompat.getDrawable(getApplicationContext(), R.drawable.splash_background);
+        splash.setBackground(launch_screen_bitmap);
+
+        return splash;
+    }
 }
